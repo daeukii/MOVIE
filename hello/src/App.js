4fd3b5c6afa0f3f4f3724ba8b3ec64e5ccfa157a
unknown <mail_gjeodnr2@gmail.com>
2023-09-19 13:05:18 +0900

    230919

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,8 +1,10 @@
+import PropTypes, { string } from "prop-types";
 import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -13,15 +15,33 @@ function App() {
     setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-    getMovies();
+    getMovies()
   }, []);
-  return <div> {loading ? <h1>Loading...</h1> : null} </div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+          <Movie 
+          key={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title} 
+          summary={movie.summary} 
+          genres={movie.genres}
+          />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App; 
+Movie.propTypes ={
+  coverImg: PropTypes.string.isRequired,
+  title:PropTypes.string.isRequired,
+  summary:PropTypes.string.isRequired,
+  genres:PropTypes.arrayOf(PropTypes,string).isRequired,
+};
+export default App;
